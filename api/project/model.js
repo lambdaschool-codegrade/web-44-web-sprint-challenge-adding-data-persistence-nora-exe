5259d8de3386232541f1d40199d8a4d3e663a858
Nora C <nocorser@gmail.com>
2021-08-13 18:54:34 -0500

    getProjects

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,11 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig');
+
+function getProjects() {
+    return db('projects as p')
+}
+
+
+module.exports = {
+    getProjects
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,6 +1,14 @@
 // build your `/api/projects` router here
 const express = require('express')
+const Projects = require('./model')
 const router = express.Router()
 
+router.get('/', (req, res, next) => {
+    Projects.getProjects()
+        .then(p => {
+            res.json(p)
+        })
+        .catch(next)
+})
 
 module.exports = router
