ad0d24aead9d5bdd87b7a77f08af47e610e37466
Nora C <nocorser@gmail.com>
2021-08-13 18:18:40 -0500

    set up server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
-// build your server here and require it from index.js
+const express = require('express');
+const server = express();
+
+const ProjectRouter = require('./project/router');
+const ResourceRouter = require('./resource/router');
+const TaskRouter = require('./task/router');
+
+server.use(express.json());
+server.use('/api/projects', ProjectRouter);
+server.use('/api/resources', ResourceRouter);
+server.use('/api/tasks', TaskRouter);
+
+module.exports = server;
